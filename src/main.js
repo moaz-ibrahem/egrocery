13f771ee070c29f923cfcbace76b6ed7b2d08919
moaz ibrahem <moazibrahem952@gmail.com>
2024-02-29 12:20:22 +0200

    templet-1

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false;
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app');
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+
+import "./scss/main.scss" ;
+import "normalize.css";
+
+createApp(App).use(store).use(router).mount("#app");
